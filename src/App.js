6783cdc9de4cb4968d3f68c1e337af778cb95b64
Ali Haider <alihaider@Alis-MacBook-Pro.local>
2021-01-11 20:07:28 +0500

    done 80% with this project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import toxicity from "@tensorflow/tfjs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default class App extends Component {
+  state = {
+    text: "",
+    loading: false,
+    prediction: [],
+  };
+
+  componentDidMount() {}
+
+  checkProbability = () => {
+    if (this.state.text.length == 0) {
+      alert("Field cannot be empty");
+      return;
+    }
+
+    this.setState({ loading: true, prediction: [] });
+    const threshold = 0.9;
+    /*global toxicity*/
+    toxicity
+      .load(threshold)
+      .then((model) => {
+        const sentences = [this.state.text];
+
+        model
+          .classify(sentences)
+          .then((prediction) => {
+            console.log(prediction);
+            this.setState({ loading: false, prediction });
+          })
+          .catch((e) => {
+            console.log("unable to classify the sentence");
+            this.setState({ loading: false });
+          });
+      })
+      .catch((e) => {
+        alert("unable to load model");
+        this.setState({ loading: false });
+      });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <center>
+          <h1 id="heading" style={{ marginTop: 20 }}>
+            Mob Detection
+          </h1>
+        </center>
+        <div style={{ marginTop: 50 }} />
+        <div
+          style={{
+            width: "50%",
+          }}
+          class="form-floating mb-3"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input
+            value={this.state.text}
+            onChange={(e) => this.setState({ text: e.target.value })}
+            type="text"
+            class="form-control"
+            placeholder="Write here!"
+            style={{ width: "100%" }}
+          />
+          <label for="floatingInput">Sentence</label>
+        </div>
+        <br />
+        <button
+          onClick={() => this.checkProbability()}
+          type="button"
+          class="btn btn-primary"
+        >
+          Search
+        </button>
+        <br />
+        <br />
+        {this.state.prediction.length > 0 ? (
+          <div
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
+            className="row"
+          >
+            <div className="col-md-6">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">type</th>
+                    <th scope="col">result</th>
+                  </tr>
+                </thead>
+                {this.state.prediction.map((item, i) => (
+                  <tr key={i}>
+                    <td>
+                      {item.label == "identity_attack"
+                        ? "Identity Atttack"
+                        : item.label == "severe_toxicity"
+                        ? "Severe Bad Language"
+                        : item.label == "sexual_explicit"
+                        ? "Sexual Explicit"
+                        : item.label}
+                    </td>
+                    <td
+                      style={{
+                        color:
+                          item.results[0]?.probabilities[0] <
+                          item.results[0]?.probabilities[1]
+                            ? "red"
+                            : "green",
+                      }}
+                    >
+                      {item.results[0]?.probabilities[0] <
+                      item.results[0]?.probabilities[1]
+                        ? "True"
+                        : "False"}
+                    </td>
+                  </tr>
+                ))}
+              </table>
+            </div>
+          </div>
+        ) : this.state.loading ? (
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
-
-export default App;
